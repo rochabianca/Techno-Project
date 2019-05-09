bc12784404a906d4726374cb917d234c18a119bd
Bianca <bianca.rocha1711@gmail.com>
2019-05-09 08:56:11 -0300

    add, remove and show items on card

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el: '#app',
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    carrinho: []
   },
   filters: {
     numeroPreco(valor) {
       return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
     }
   },
   methods: {
@@ -31,6 +43,18 @@ const vm = new Vue({
     },
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({
+        id,
+        nome,
+        preco
+      });
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
     }
   },
   created() {
